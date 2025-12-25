22c4c5c076e32a69ac26d4cc2f8e086468f5c32e
dheeraj-varshney <dheeraj.varshney@harness.io>
2025-12-25 22:31:01 +0530

    Update main.go

--- main.go
+++ main.go
@@ -7,19 +7,17 @@ import (
 	"os/exec"
 )
 
-//go:embed posix/script
-var script string
+//go:embed sto-plugin-binary
+var stoBinary []byte
 
 func main() {
-	// Write embedded script to temp file
-	tmpFile := "/tmp/sto-script.sh"
-	if err := os.WriteFile(tmpFile, []byte(script), 0755); err != nil {
-		fmt.Fprintf(os.Stderr, "Error writing script: %v\n", err)
+	tmpBinary := "/tmp/sto-plugin-bin"
+	if err := os.WriteFile(tmpBinary, stoBinary, 0755); err != nil {
+		fmt.Fprintf(os.Stderr, "Error writing binary: %v\n", err)
 		os.Exit(1)
 	}
 
-	// Execute the script
-	cmd := exec.Command("/bin/sh", tmpFile)
+	cmd := exec.Command(tmpBinary, "--run-strategy", "single-container")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Env = os.Environ()
@@ -28,7 +26,6 @@ func main() {
 		if exitErr, ok := err.(*exec.ExitError); ok {
 			os.Exit(exitErr.ExitCode())
 		}
-		fmt.Fprintf(os.Stderr, "Error executing script: %v\n", err)
 		os.Exit(1)
 	}
 }
